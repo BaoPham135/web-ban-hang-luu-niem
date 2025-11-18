4a80ebff0de843d2ef826770920c2c96bd601d65
BaoPham135 <hoaibao13578@gmail.com>
2025-11-18 23:35:30 +0700

    Add files via upload

---
+++ web_mysqli.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 18, 2025 at 05:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `web_mysqli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_admin`
+--
+
+CREATE TABLE `tbl_admin` (
+  `id_admin` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `admin_status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_admin`
+--
+
+INSERT INTO `tbl_admin` (`id_admin`, `username`, `password`, `admin_status`) VALUES
+(1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_dangky`
+--
+
+CREATE TABLE `tbl_dangky` (
+  `id_dangky` int(11) NOT NULL,
+  `hovaten` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `dienthoai` varchar(200) NOT NULL,
+  `matkhau` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_dangky`
+--
+
+INSERT INTO `tbl_dangky` (`id_dangky`, `hovaten`, `email`, `dienthoai`, `matkhau`) VALUES
+(10, 'Phạm Hoài Bảo', 'bao@gmail.com', '0797150106', '81dc9bdb52d04dc20036dbd8313ed055');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_danhmuc`
+--
+
+CREATE TABLE `tbl_danhmuc` (
+  `id_danhmuc` int(11) NOT NULL,
+  `tendanhmuc` varchar(100) NOT NULL,
+  `thutu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_danhmuc`
+--
+
+INSERT INTO `tbl_danhmuc` (`id_danhmuc`, `tendanhmuc`, `thutu`) VALUES
+(4, 'Mèo thần tài', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_khachhang`
+--
+
+CREATE TABLE `tbl_khachhang` (
+  `id_khachhang` int(11) NOT NULL,
+  `tenkhachhang` varchar(200) NOT NULL,
+  `dienthoai` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `diachi` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_khachhang`
+--
+
+INSERT INTO `tbl_khachhang` (`id_khachhang`, `tenkhachhang`, `dienthoai`, `email`, `diachi`) VALUES
+(5, 'Phạm Hoài Bảo', '0797150106', 'hoaibao13578@gmail.com', 'B66/3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sanpham`
+--
+
+CREATE TABLE `tbl_sanpham` (
+  `id_sanpham` int(11) NOT NULL,
+  `tensanpham` varchar(250) NOT NULL,
+  `masp` varchar(100) NOT NULL,
+  `giasp` varchar(50) NOT NULL,
+  `soluong` int(11) NOT NULL,
+  `noidung` text NOT NULL,
+  `tomtat` tinytext NOT NULL,
+  `hinhanh` varchar(50) NOT NULL,
+  `tinhtrang` int(11) NOT NULL,
+  `id_danhmuc` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_sanpham`
+--
+
+INSERT INTO `tbl_sanpham` (`id_sanpham`, `tensanpham`, `masp`, `giasp`, `soluong`, `noidung`, `tomtat`, `hinhanh`, `tinhtrang`, `id_danhmuc`) VALUES
+(9, 'Mèo thần tài', '001', '250000', 3, 'ádasd', 'đâs', '1762283758_RobloxScreenShot20241230_153706940.png', 1, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  ADD PRIMARY KEY (`id_dangky`);
+
+--
+-- Indexes for table `tbl_danhmuc`
+--
+ALTER TABLE `tbl_danhmuc`
+  ADD PRIMARY KEY (`id_danhmuc`);
+
+--
+-- Indexes for table `tbl_khachhang`
+--
+ALTER TABLE `tbl_khachhang`
+  ADD PRIMARY KEY (`id_khachhang`);
+
+--
+-- Indexes for table `tbl_sanpham`
+--
+ALTER TABLE `tbl_sanpham`
+  ADD PRIMARY KEY (`id_sanpham`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_admin`
+--
+ALTER TABLE `tbl_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_dangky`
+--
+ALTER TABLE `tbl_dangky`
+  MODIFY `id_dangky` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbl_danhmuc`
+--
+ALTER TABLE `tbl_danhmuc`
+  MODIFY `id_danhmuc` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_khachhang`
+--
+ALTER TABLE `tbl_khachhang`
+  MODIFY `id_khachhang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_sanpham`
+--
+ALTER TABLE `tbl_sanpham`
+  MODIFY `id_sanpham` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
